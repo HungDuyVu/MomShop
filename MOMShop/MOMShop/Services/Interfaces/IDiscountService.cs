503b3f71ff92b8ccaa3b13ed98e42c6b5a87932d
HungVu <vuhung.vtq@gmail.com>
2025-01-04 17:00:17 +0700

    first commit

---
+++ MOMShop/MOMShop/Services/Interfaces/IDiscountService.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using MOMShop.Dto.Discount;
+
+namespace MOMShop.Services.Interfaces
+{
+    public interface IDiscountService
+    {
+        DiscountDto Add(DiscountDto input);
+        DiscountDto Update(DiscountDto input);
+        void Delete (int id);
+        List<DiscountDto> GetAll(FilterDiscountDto input);
+        List<DiscountDto> GetAllByStatus(int status);
+        DiscountDto Find(int id);
+        string CheckDiscountCodeExist(string discountCode);
+        int CheckDiscountPercent(string discountCode);
+    }
+}
---
+++ MomShopFE/src/models/user.ts
@@ -0,0 +1,15 @@
+export class UserDto {
+  id: number;
+  email: string;
+  password: string;
+  phone: string;
+  fullName: string;
+  birthDay: Date;
+  gender: string;
+  address: string;
+  province: string;
+  district: string;
+  nation: string;
+  createdBy: string;
+  userType : number
+}
